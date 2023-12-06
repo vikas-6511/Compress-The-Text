c6f16edf66b7ea5d85c0d7ed7b6f1db111bc5106
Vikas Yadav <yvikas8340@gmail.com>
2023-12-07 02:42:18 +0530

    second commit

---
+++ encode.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "huffman.hpp"
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        cout << "Failed to detect Files";
+		exit(1);
+	}
+    
+    huffman f(argv[1], argv[2]);
+    f.compress();
+    // control goes to 3rd line in huffman.cpp
+    cout << "Compressed successfully" << endl;
+    
+    return 0;
+}
